aeb22c63782b346e2e7e86f386a0679823d7f27e
Jose Munoz <MunozCriolloJJ@cardiff.ac.uk>
2025-02-21 09:48:50 +0000

    some fixes to configuration files to optimise resources and point to reservation

--- run_nextflow.sh
+++ run_nextflow.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
-
-#SBATCH -p htc
-#SBATCH --mem=80GB
+#SBATCH -p compute_amd
+#SBATCH --mem=10GB
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=1
 #SBATCH --tasks-per-node=1
-#SBATCH -t 08:00:00 
+#SBATCH -t 02:00:00 
 #SBATCH -o slurm/%J.out
 #SBATCH -e slurm/%J.err
 #SBATCH --job-name=NEXT
-#SBATCH --account=scw1448
+#SBATCH --account=scw1001
+#SBATCH --reservation=training-nfcore-test-amd
 
 myDir=$(pwd)
 
@@ -17,12 +17,8 @@ module load java
 module load nextflow/24.10.4
 module load cellranger/7.2.0
 
-#/scratch/c.mcbsd1/nf-core/scrnaseq/bin/nextflow-24.10.2/nextflow run nf-core/scrnaseq -profile singularity --input samplesheet.csv --cellranger_index /scratch/c.mcbsd1/nf-core/scrnaseq/resources/refdata-gex-mm10-2020-A --protocol 10XV2 --aligner cellranger --outdir ../output
-
-nextflow run ${myDir}/main_dsl2_new2_final.nf --aggr true -with-trace
+nextflow run ${myDir}/main.nf --aggr true -with-trace
 
 
-# to run
+# to run this script
 # sbatch run_nextflow.sh
-
-
